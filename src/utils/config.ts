431d8ba1113ab9d5bddc2d5c95c61def8de7dcde
adityashetty35 <92563416+adityashetty35@users.noreply.github.com>
2024-02-08 10:37:22 +0530

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,7 +3,7 @@ import type Config from "../types/Config";
 const config: Config = {
   lapaAuthenticationProtocol:"http",
   lapaAuthenticationIp:"localhost",
-  lapaAuthenticationPort:10011,
+  lapaAuthenticationPort:"10011",
 };
 
 export default config;
